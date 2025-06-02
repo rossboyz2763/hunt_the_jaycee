aa54a28a7ca71814a359ffefa0422b71f9ef023e
rossboyz <blade.ross@education.nsw.gov.au>
2025-06-02 01:06:05 +0000

    wrote the dictionary and tested linked_caves

--- cave.py
+++ cave.py
@@ -2,3 +2,27 @@ class Cave:
     def __init__(self, cave_name):
         self.name = cave_name
         self.description = None
+        self.linked_caves = {}
+    
+    def set_name(self, cave_name):
+        self.name = cave_name
+
+    def get_name(self):
+        return self.name
+
+    def set_description(self, cave_description):
+        self.description = cave_description
+
+    def get_description(self):
+        return self.description
+
+    def describe(self):
+        print(self.description)
+
+    def link_caves(self, cave_to_link, direction):
+        self.linked_caves[direction] = cave_to_link
+        print(self.name + " linked caves:" + repr(self.linked_caves))
+
+    def get_details(self):
+        for direction, cave in self.linked_caves.items():
+            print("The " + cave.get_name() + " is " + direction)
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from cave import Cave
 
 cavern = Cave("Cavern")
+cavern.set_description("A deep and dirty cave.")
+grotto = Cave("Grotto")
+grotto.set_description("A small cave with ancient markings.")
+dungeon = Cave("Dungeon")
+dungeon.set_description("A large cave with a rack.")
+
+cavern.link_caves(dungeon, "South")
+dungeon.link_caves(cavern, "North")
+dungeon.link_caves(grotto, "West")
+grotto.link_caves(dungeon, "East")
+
+cavern.get_details()
+dungeon.get_details()
+grotto.get_details()
